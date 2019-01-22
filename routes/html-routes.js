ed258ea5b2248fcac8d1073d45d7753f6a3b4d7f
unknown <wroehrman@yahoo.com>
2019-01-21 21:30:30 -0600

    updates to routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,21 +10,17 @@ module.exports = function (app) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
-  app.get("/portfolio", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/portfolio.html"));
-  });
+  // app.get("/portfolio", function (req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/portfolio.html"));
+  // });
 
-  app.get("/home", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
+  // app.get("/contact", function (req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/contact.html"));
+  // });
 
-  app.get("/contact", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/contact.html"));
-  });
-
-  app.get("/resume", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/resume.html"));
-  });
+  // app.get("/resume", function (req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/resume.html"));
+  // });
 
   
   

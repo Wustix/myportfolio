30deef3a5aaadb86a36e13ecea2039c523e57184
unknown <wroehrman@yahoo.com>
2019-01-31 14:29:10 -0600

    updates to links and portfolio projects

--- config/connection.js
+++ config/connection.js
@@ -9,12 +9,13 @@ var Sequelize = require("sequelize");
 var sequelize = new Sequelize("message", "root", "Otis13jhawkhalo", {
   host: "localhost",
   port: 3306,
-  dialect: "mysql",
+  dialect: "mysql",  
   pool: {
     max: 5,
     min: 0,
     idle: 10000
-  }
+  },
+  operatorsAliases: false // disable aliases
 });
 
 // Exports the connection for other files to use
